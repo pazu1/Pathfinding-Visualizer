874414db8e68abe59fced4523c1d8aa01189964c
pazu1 <pazu1s@outlook.com>
2020-09-01 11:55:30 +0300

    Updates

---
+++ precache-manifest.991830fc290d60ee3a488e3757142c5a.js
@@ -0,0 +1,31 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+    {
+        revision: "8c46a1205bf9badbffc2c36f3ea6aa50",
+        url: "/Pathfinding-Visualizer/index.html",
+    },
+    {
+        revision: "e4f91048b181d2edb82e",
+        url: "/Pathfinding-Visualizer/static/css/2.d219d102.chunk.css",
+    },
+    {
+        revision: "ae53e85ad725ff273341",
+        url: "/Pathfinding-Visualizer/static/css/main.aa8c37be.chunk.css",
+    },
+    {
+        revision: "e4f91048b181d2edb82e",
+        url: "/Pathfinding-Visualizer/static/js/2.492087eb.chunk.js",
+    },
+    {
+        revision: "33453ffae155fa5ef37c3df6e3f04d0b",
+        url:
+            "/Pathfinding-Visualizer/static/js/2.492087eb.chunk.js.LICENSE.txt",
+    },
+    {
+        revision: "ae53e85ad725ff273341",
+        url: "/Pathfinding-Visualizer/static/js/main.dd591add.chunk.js",
+    },
+    {
+        revision: "e2c724341a9b683045d0",
+        url: "/Pathfinding-Visualizer/static/js/runtime-main.3c3fb3bc.js",
+    },
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,16 +11,18 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
 importScripts(
-  "/Pathfinding-Visualizer/precache-manifest.02ad21ec357ca25e2c3cb564857c9d57.js"
+    "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
+importScripts(
+    "/Pathfinding-Visualizer/precache-manifest.991830fc290d60ee3a488e3757142c5a.js"
+);
+
+self.addEventListener("message", (event) => {
+    if (event.data && event.data.type === "SKIP_WAITING") {
+        self.skipWaiting();
+    }
 });
 
 workbox.core.clientsClaim();
@@ -33,7 +35,9 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Pathfinding-Visualizer/index.html"), {
-  
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
-});
+workbox.routing.registerNavigationRoute(
+    workbox.precaching.getCacheKeyForURL("/Pathfinding-Visualizer/index.html"),
+    {
+        blacklist: [/^\/_/, /\/[^/?]+\.[^/]+$/],
+    }
+);
